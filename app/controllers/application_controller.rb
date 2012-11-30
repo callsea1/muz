55a9798009ded3dcb9b1e91130eef72e85390b29
Roberto Simões <me@robertosimoes.net>
2012-11-29 18:07:20 -0600

    added styling~

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,18 @@ class ApplicationController < ActionController::Base
   
 
 
-      def after_sign_in_path_for(resource)                                                                                                                      
-        sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')                                            
-        if (request.referer == sign_in_url)                                                                                                                     
-          super                                                                                                                                                 
-        else                                                                                                                                                    
-          request.referer || stored_location_for(resource) || root_path                                                                                         
-        end                                                                                                                                                     
-      end  
-
-      def after_sign_in_path_for(resource)
-          return request.env['omniauth.origin'] || stored_location_for(resource) || root_path
-      end       
+      # def after_sign_in_path_for(resource)                                                                                                                      
+#         sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')                                            
+#         if (request.referer == sign_in_url)                                                                                                                     
+#           super                                                                                                                                                 
+#         else                                                                                                                                                    
+#           request.referer || stored_location_for(resource) || user_home_path()                                                                                         
+#         end                                                                                                                                                     
+#       end  
+# 
+#       def after_sign_in_path_for(resource)
+#           return request.env['omniauth.origin'] || stored_location_for(resource) || user_home_path()
+#       end       
       
   
 end
--- app/controllers/welcome_pages_controller.rb
+++ app/controllers/welcome_pages_controller.rb
@@ -1,9 +1,13 @@
 class WelcomePagesController < ApplicationController
-	before_filter :authenticate_user!
+	#before_filter :authenticate_user!
 
 	def home
 	  @welcome = "Welcome to MusicMind-studio"
-      @current_user_song_rooms = current_user.song_rooms.all
+    @current_user_song_rooms = current_user.song_rooms.all
 	  @collab_requests = Collaborator.where(:user_id => current_user.id)
 	end
+	
+	def index
+	end
+	
 end
