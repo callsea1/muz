c4db5fa9a6fb0cbcb43fc2c9411e16683458773e
Joshua Chestang <joshua.chestang@gmail.com>
2012-12-04 18:30:55 -0600

    added songs/tracks/song rooms to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   
   def show
   	@user = User.find(params[:id])
+  	@song_rooms = SongRoom.find_all_by_id(@user.collaborators.map(&:song_room_id))
+  	@user_song_uploads = @user.user_song_uploads
+  	@versions = @user.versions
+
   end
   
   def index
